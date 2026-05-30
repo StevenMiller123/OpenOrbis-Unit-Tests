86ad8c93c892cfe28a0a9edee014e835a45c7dab
Stephen Miller <56742918+StevenMiller123@users.noreply.github.com>
2026-05-29 20:24:56 -0500

    Weird file mmap tests
    Writing these to test stuff for shadPS4's upcoming memory rewrite, found fpPS4 has some issues too 😄

--- tests/code/memory_test/code/test_550.cpp
+++ tests/code/memory_test/code/test_550.cpp
@@ -191,3 +191,220 @@ TEST(MemoryTests, TLOU2Test) {
   result = sceKernelCheckedReleaseDirectMemory(first_phys_addr, 0x100000000);
   UNSIGNED_INT_EQUALS(0, result);
 }
+
+TEST(MemoryTests, TestFileMappings) {
+  char zeros_buf[0x4000];
+  memset(zeros_buf, 0, sizeof(zeros_buf));
+  char ones_buf[0x4000];
+  memset(ones_buf, 1, sizeof(ones_buf));
+  char twos_buf[0x4000];
+  memset(twos_buf, 2, sizeof(twos_buf));
+  int32_t MAP_NOFLAGS = 0;
+  int32_t MAP_SHARED = 1;
+  int32_t MAP_PRIVATE = 2;
+
+  auto open_file = [](int32_t flags) {
+    int32_t fd = sceKernelOpen("/data/file_map_test.txt", flags, 0777);
+    CHECK(fd > 0);
+    return fd;
+  };
+
+  auto close_file = [](int32_t fd) {
+    int32_t result = sceKernelClose(fd);
+    UNSIGNED_INT_EQUALS(0, result);
+  };
+
+  auto map_file = [](uint64_t addr, int32_t fd, int32_t flags, int32_t prot) {
+    // Map the file to memory
+    uint64_t out_addr = addr;
+    int32_t  result   = sceKernelMmap(out_addr, 0x4000, prot, flags, fd, 0, &out_addr);
+    UNSIGNED_INT_EQUALS(0, result);
+    return out_addr;
+  };
+
+  auto unmap_file = [](uint64_t addr) {
+    int32_t result = sceKernelMunmap(addr, 0x4000);
+    UNSIGNED_INT_EQUALS(0, result);
+  };
+
+  auto write_file = [](int32_t fd, void* addr) {
+    // Write contents from addr to file
+    int64_t result = sceKernelWrite(fd, addr, 0x4000);
+    UNSIGNED_INT_EQUALS(0x4000, result);
+    // Reset file pointer
+    result = sceKernelLseek(fd, 0, 0);
+    UNSIGNED_INT_EQUALS(0, result);
+  };
+
+  auto trunc_file = [](int32_t fd, uint64_t size) {
+    // Truncate file to requested size
+    int64_t result = sceKernelFtruncate(fd, size);
+    UNSIGNED_INT_EQUALS(0, result);
+    // Reset file pointer
+    result = sceKernelLseek(fd, 0, 0);
+    UNSIGNED_INT_EQUALS(0, result);
+  };
+
+  auto check_memory = [](uint64_t addr, void* data) {
+    int32_t result = memcmp((void*)addr, data, 0x4000);
+    UNSIGNED_INT_EQUALS(0, result);
+  };
+
+  auto write_memory = [](uint64_t addr, void* data) {
+    memcpy((void*)addr, data, 0x4000);
+  };
+
+  auto protect_memory = [](uint64_t addr, int32_t prot) {
+    int32_t result = sceKernelMprotect(addr, 0x4000, prot);
+    UNSIGNED_INT_EQUALS(0, result);
+  };
+
+  auto check_file = [](int32_t fd, void* data) {
+    // Read data to addr
+    char addr[0x4000];
+    int64_t result = sceKernelRead(fd, addr, 0x4000);
+    UNSIGNED_INT_EQUALS(0x4000, result);
+
+    // Reset file pointer
+    result = sceKernelLseek(fd, 0, 0);
+    UNSIGNED_INT_EQUALS(0, result);
+
+    // Check memory contents
+    result = memcmp(addr, data, 0x4000);
+    UNSIGNED_INT_EQUALS(0, result);
+  };
+
+  // Open and prepare file
+  int32_t fd = open_file(0x602);
+  trunc_file(fd, 0x4000);
+
+  // File data starts zeroed after ftruncate
+  check_file(fd, zeros_buf);
+
+  // Write ones for testing purposes
+  write_file(fd, ones_buf);
+
+  // Close file and begin tests.
+  close_file(fd);
+
+  // A couple combinations of file perms and mmap prots need testing here.
+  // First, we want the basic case. Open file as read-write, mmap as read-write with MAP_SHARED specified.
+  fd = open_file(2);
+  uint64_t addr = map_file(0, fd, MAP_SHARED, 3);
+
+  // What we should have here is a shared mapping.
+  // All writes to the memory will be visible in the file, and all writes to the file will appear in memory.
+  // As-is, both memory and file should be full of ones.
+  check_memory(addr, ones_buf);
+  check_file(fd, ones_buf);
+
+  // Now write to the memory.
+  write_memory(addr, twos_buf);
+
+  // Now reads should show 2s
+  check_file(fd, twos_buf);
+
+  // Memory should (naturally) also contain twos
+  check_memory(addr, twos_buf);
+
+  // Writing directly to the file should update the memory
+  write_file(fd, ones_buf);
+
+  // Now reads should (naturally) show 1s
+  check_file(fd, ones_buf);
+
+  // Memory should also contain ones
+  check_memory(addr, ones_buf);
+
+  // Unmap and close file
+  unmap_file(addr);
+  close_file(fd);
+
+  fd = open_file(2);
+  addr = map_file(0, fd, MAP_PRIVATE, 3);
+
+  // What we should have here is a private mapping, where file contents and memory contents aren't synchronized
+  // As-is, both memory and file should be full of ones.
+  check_memory(addr, ones_buf);
+  check_file(fd, ones_buf);
+
+  // Writing directly to the file should not update the memory
+  write_file(fd, twos_buf);
+
+  // Now reads should show twos
+  check_file(fd, twos_buf);
+
+  // Memory should still contain ones
+  check_memory(addr, ones_buf);
+
+  // Unmap file
+  unmap_file(addr);
+
+  addr = map_file(0, fd, MAP_PRIVATE, 3);
+
+  // Now memory should contain twos
+  check_memory(addr, twos_buf);
+
+  // Writes to memory won't update the file either
+  write_memory(addr, ones_buf);
+
+  // Memory should contain ones
+  check_memory(addr, ones_buf);
+
+  // File should still contain twos
+  check_file(fd, twos_buf);
+
+  // Write ones
+  write_file(fd, ones_buf);
+
+  // Unmap and close file
+  unmap_file(addr);
+  close_file(fd);
+
+  // Open file as write-only.
+  fd = open_file(1);
+
+  // Read-write mmap is impossible.
+  int32_t result = sceKernelMmap(0, 0x4000, 3, MAP_SHARED, fd, 0, &addr);
+  UNSIGNED_INT_EQUALS(ORBIS_KERNEL_ERROR_EACCES, result);
+  result = sceKernelMmap(0, 0x4000, 3, MAP_PRIVATE, fd, 0, &addr);
+  UNSIGNED_INT_EQUALS(ORBIS_KERNEL_ERROR_EACCES, result);
+  result = sceKernelMmap(0, 0x4000, 3, MAP_NOFLAGS, fd, 0, &addr);
+  UNSIGNED_INT_EQUALS(ORBIS_KERNEL_ERROR_EACCES, result);
+
+  // Write-only mmap is impossible
+  result = sceKernelMmap(0, 0x4000, 2, MAP_SHARED, fd, 0, &addr);
+  UNSIGNED_INT_EQUALS(ORBIS_KERNEL_ERROR_EACCES, result);
+  result = sceKernelMmap(0, 0x4000, 2, MAP_PRIVATE, fd, 0, &addr);
+  UNSIGNED_INT_EQUALS(ORBIS_KERNEL_ERROR_EACCES, result);
+  result = sceKernelMmap(0, 0x4000, 2, MAP_NOFLAGS, fd, 0, &addr);
+  UNSIGNED_INT_EQUALS(ORBIS_KERNEL_ERROR_EACCES, result);
+
+  // Read-only mmap is impossible
+  result = sceKernelMmap(0, 0x4000, 1, MAP_SHARED, fd, 0, &addr);
+  UNSIGNED_INT_EQUALS(ORBIS_KERNEL_ERROR_EACCES, result);
+  result = sceKernelMmap(0, 0x4000, 1, MAP_PRIVATE, fd, 0, &addr);
+  UNSIGNED_INT_EQUALS(ORBIS_KERNEL_ERROR_EACCES, result);
+  result = sceKernelMmap(0, 0x4000, 1, MAP_NOFLAGS, fd, 0, &addr);
+  UNSIGNED_INT_EQUALS(ORBIS_KERNEL_ERROR_EACCES, result);
+
+  // No-access mmap is possible.
+  addr = map_file(0, fd, MAP_SHARED, 0);
+
+  // We can protect this to get access to data
+  protect_memory(addr, 3);
+
+  // Now memory should contain ones if it is properly backed
+  check_memory(addr, ones_buf);
+
+  // Since this is a shared mapping, we can write twos to the file and see the change
+  write_file(fd, twos_buf);
+  check_memory(addr, twos_buf);
+
+  // Bring back ones before unmapping
+  write_file(fd, ones_buf);
+  
+  // Unmap and close file
+  unmap_file(addr);
+  close_file(fd);
+}
